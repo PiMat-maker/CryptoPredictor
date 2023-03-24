093de66cee26c3d6071fbfc1a7a12d0e7809dd74
PiMat-maker <54264954+PiMat-maker@users.noreply.github.com>
2023-03-24 20:15:48 +0300

    Change imports

--- DataAnalysis/plots/time_diagrams.py
+++ DataAnalysis/plots/time_diagrams.py
@@ -1,15 +1,19 @@
 import seaborn as sns
 
-import error
+from error import WrongArgsError
+from plots.utils import (
+    save_plot_decorator,
+    annotate_plot
+)
 
 
 @save_plot_decorator
 def heatmap(df, *columns, rows_limit=40):
     if len(columns) != 3:
-        raise error.WrongArgsError(f"Wrong number of columns: {len(columns)}. Should be 3")
+        raise WrongArgsError(f"Wrong number of columns: {len(columns)}. Should be 3")
     permitted_types = ["int64", "float64"]
     if df[columns[2]].dtype not in permitted_types:
-        raise error.WrongArgsError(f"Wrong type of last column: {df[columns[2]].dtype}. Valid types: {permitted_types}")
+        raise WrongArgsError(f"Wrong type of last column: {df[columns[2]].dtype}. Valid types: {permitted_types}")
 
     heatmap_df = df.pivot(index=columns[0], columns=columns[1], values=columns[2])
     heatmap_df[[heatmap_df.isna()]] = 0
