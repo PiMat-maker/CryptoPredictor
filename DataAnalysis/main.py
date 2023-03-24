863706e0f8d77d2ca9292f6115a1ceee67193af2
PiMat-maker <54264954+PiMat-maker@users.noreply.github.com>
2023-03-24 14:41:14 +0300

    Add main file

---
+++ DataAnalysis/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+import aggregate.time as agg_t
+import plots.time_diagrams as td
+
+
+def main():
+    df = pd.read_csv("../DataMining/binance_data.csv")
+    df = agg_t.agg_to_hour_data(df)
+    df["hour"] = df.date.agg(lambda x: x.hour)
+    df["day_date"] = df.date.agg(lambda x: x.dt.date)
+    td.heatmap(df, "day_date", "hour", "volume")
+
+
+if __name__ == '__main__':
+    main()
