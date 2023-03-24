8d616f53d8b263751dc91858aedb4b71e0c9ca51
PiMat-maker <54264954+PiMat-maker@users.noreply.github.com>
2023-03-25 02:56:12 +0300

    Add field annot for heatmap

--- DataAnalysis/plots/time_diagrams.py
+++ DataAnalysis/plots/time_diagrams.py
@@ -8,7 +8,7 @@ from plots.utils import (
 
 
 @save_plot_decorator
-def heatmap(df, *columns, rows_limit=40):
+def heatmap(df, *columns, rows_limit=40, annot=False):
     if len(columns) != 3:
         raise WrongArgsError(f"Wrong number of columns: {len(columns)}. Should be 3")
     permitted_types = ["int64", "float64"]
@@ -18,7 +18,7 @@ def heatmap(df, *columns, rows_limit=40):
     heatmap_df = df.pivot(index=columns[0], columns=columns[1], values=columns[2])
     heatmap_df[[heatmap_df.isna()]] = 0
 
-    sns.heatmap(heatmap_df[:rows_limit], linewidths=.5)
+    sns.heatmap(heatmap_df[-rows_limit:], linewidths=.5, annot=annot, fmt=".1f")
     annotate_plot(
         title=f"{columns[2].capitalize()} by {columns[1]}",
         x_label=columns[1],
