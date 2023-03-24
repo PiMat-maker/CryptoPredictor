b3df612854eb82baea925eb136c745eb61976495
PiMat-maker <54264954+PiMat-maker@users.noreply.github.com>
2023-03-24 14:58:09 +0300

    Drop duplicates in dataframe before analysis

--- DataAnalysis/main.py
+++ DataAnalysis/main.py
@@ -6,6 +6,7 @@ import plots.time_diagrams as td
 
 def main():
     df = pd.read_csv("../DataMining/binance_data.csv")
+    df.drop_duplicates(inplace=True)
     df = agg_t.agg_to_hour_data(df)
     df["hour"] = df.date.agg(lambda x: x.hour)
     df["day_date"] = df.date.agg(lambda x: x.dt.date)
